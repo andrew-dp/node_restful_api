0cf798e53a821a6fe62786079abb5cc08392f903
Andrew Guard <aguard@digitalprimates.net>
2014-06-04 13:44:19 -0500

    first commit

---
+++ express.js
@@ -0,0 +1,60 @@
+var express = require('express')
+  , mongoskin = require('mongoskin')
+  , bodyParser = require('body-parser')
+
+var app = express()
+app.use(bodyParser())
+
+var db = mongoskin.db('mongodb://@localhost:27017/peopledatabase', {safe:true})
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+app.param('collectionName', function(req, res, next, collectionName){
+  req.collection = db.collection(collectionName)
+  return next()
+})
+
+app.get('/', function(req, res, next) {
+  res.send('please select a collection, e.g., /collections/messages')
+})
+
+app.get('/:collectionName', function(req, res, next) {
+  req.collection.find({} ,{limit:5000, sort: [['_id',-1]]}).toArray(function(e, results){
+    if (e) return next(e)
+    res.send(results)
+  })
+})
+
+app.post('/:collectionName', function(req, res, next) {
+  req.collection.insert(req.body, {}, function(e, results){
+    if (e) return next(e)
+    res.send(results)
+  })
+})
+
+app.get('/:collectionName/:id', function(req, res, next) {
+  req.collection.findById(req.params.id, function(e, result){
+    if (e) return next(e)
+    res.send(result)
+  })
+})
+
+app.put('/:collectionName/:id', function(req, res, next) {
+  req.collection.updateById(req.params.id, {$set:req.body}, {safe:true, multi:false}, function(e, result){
+    if (e) return next(e)
+    res.send((result===1)?{msg:'success'}:{msg:'error'})
+  })
+})
+
+app.del('/:collectionName/:id', function(req, res, next) {
+  req.collection.removeById(req.params.id, function(e, result){
+    if (e) return next(e)
+    res.send((result===1)?{msg:'success'}:{msg:'error'})
+  })
+})
+
+app.listen(3000)
