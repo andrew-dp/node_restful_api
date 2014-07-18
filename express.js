5040df5e198d184724b18202ddc70af6f1bc007e
Andrew Guard <aguard@digitalprimates.net>
2014-07-18 10:12:36 -0500

    first commit

--- express.js
+++ express.js
@@ -1,13 +1,13 @@
 var express = require('express')
   , mongoskin = require('mongoskin')
   , bodyParser = require('body-parser')
-  , cors = require('cors')
+  , cors = require('cors');
 
-var app = express()
-app.use(bodyParser())
-app.use(cors())
+var app = express();
+app.use(bodyParser());
+app.use(cors());
 
-var db = mongoskin.db('mongodb://@localhost:27017/peopledatabase', {safe:true})
+var db = mongoskin.db('mongodb://@localhost:27017/peopledatabase', {safe:true});
 
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -16,47 +16,47 @@ app.all('/*', function(req, res, next) {
 });
 
 app.param('collectionName', function(req, res, next, collectionName){
-  req.collection = db.collection(collectionName)
+  req.collection = db.collection(collectionName);
   return next()
-})
+});
 
 app.get('/', function(req, res, next) {
   res.send('please select a collection, e.g., /collections/messages')
-})
+});
 
 app.get('/:collectionName', function(req, res, next) {
   req.collection.find({} ,{limit:500, sort: [['_id',-1]]}).toArray(function(e, results){
-    if (e) return next(e)
+    if (e) return next(e);
     res.send(results)
   })
-})
+});
 
 app.post('/:collectionName', function(req, res, next) {
   req.collection.insert(req.body, {}, function(e, results){
-    if (e) return next(e)
+    if (e) return next(e);
     res.send(results)
   })
-})
+});
 
 app.get('/:collectionName/:id', function(req, res, next) {
   req.collection.findById(req.params.id, function(e, result){
-    if (e) return next(e)
+    if (e) return next(e);
     res.send(result)
   })
-})
+});
 
 app.put('/:collectionName/:id', function(req, res, next) {
   req.collection.updateById(req.params.id, {$set:req.body}, {safe:true, multi:false}, function(e, result){
-    if (e) return next(e)
+    if (e) return next(e);
     res.send((result===1)?{msg:'success'}:{msg:'error'})
   })
-})
+});
 
 app.del('/:collectionName/:id', function(req, res, next) {
   req.collection.remove({'id':parseInt(req.params.id)}, function(e, result){
-    if (e) return next(e)
+    if (e) return next(e);
     res.send((result===1)?{msg:'success'}:{msg:'error'})
   })
-})
+});
 
-app.listen(3000)
+app.listen(3000);
--- express.test.js
+++ express.test.js
@@ -1,8 +1,8 @@
-var superagent = require('superagent')
-var expect = require('expect.js')
+var superagent = require('superagent');
+var expect = require('expect.js');
 
 describe('express rest api server', function(){
-  var id
+  var id;
 
   it('post object', function(done){
     superagent.post('http://localhost:3000/collections/test')
@@ -11,36 +11,36 @@ describe('express rest api server', function(){
       })
       .end(function(e,res){
         // console.log(res.body)
-        expect(e).to.eql(null)
-        expect(res.body.length).to.eql(1)
-        expect(res.body[0]._id.length).to.eql(24)
-        id = res.body[0]._id
+        expect(e).to.eql(null);
+        expect(res.body.length).to.eql(1);
+        expect(res.body[0]._id.length).to.eql(24);
+        id = res.body[0]._id;
         done()
       })
-  })
+  });
 
   it('retrieves an object', function(done){
     superagent.get('http://localhost:3000/collections/test/'+id)
       .end(function(e, res){
         // console.log(res.body)
-        expect(e).to.eql(null)
-        expect(typeof res.body).to.eql('object')
-        expect(res.body._id.length).to.eql(24)
-        expect(res.body._id).to.eql(id)
+        expect(e).to.eql(null);
+        expect(typeof res.body).to.eql('object');
+        expect(res.body._id.length).to.eql(24);
+        expect(res.body._id).to.eql(id);
         done()
       })
-  })
+  });
 
   it('retrieves a collection', function(done){
     superagent.get('http://localhost:3000/collections/test')
       .end(function(e, res){
         // console.log(res.body)
-        expect(e).to.eql(null)
-        expect(res.body.length).to.be.above(0)
-        expect(res.body.map(function (item){return item._id})).to.contain(id)
+        expect(e).to.eql(null);
+        expect(res.body.length).to.be.above(0);
+        expect(res.body.map(function (item){return item._id})).to.contain(id);
         done()
       })
-  })
+  });
 
   it('updates an object', function(done){
     superagent.put('http://localhost:3000/collections/test/'+id)
@@ -48,33 +48,33 @@ describe('express rest api server', function(){
         , email: 'peter@yahoo.com'})
       .end(function(e, res){
         // console.log(res.body)
-        expect(e).to.eql(null)
-        expect(typeof res.body).to.eql('object')
-        expect(res.body.msg).to.eql('success')
+        expect(e).to.eql(null);
+        expect(typeof res.body).to.eql('object');
+        expect(res.body.msg).to.eql('success');
         done()
       })
-  })
+  });
 
   it('checks an updated object', function(done){
     superagent.get('http://localhost:3000/collections/test/'+id)
       .end(function(e, res){
         // console.log(res.body)
-        expect(e).to.eql(null)
-        expect(typeof res.body).to.eql('object')
-        expect(res.body._id.length).to.eql(24)
-        expect(res.body._id).to.eql(id)
-        expect(res.body.name).to.eql('Peter')
+        expect(e).to.eql(null);
+        expect(typeof res.body).to.eql('object');
+        expect(res.body._id.length).to.eql(24);
+        expect(res.body._id).to.eql(id);
+        expect(res.body.name).to.eql('Peter');
         done()
       })
-  })
+  });
   it('removes an object', function(done){
     superagent.del('http://localhost:3000/collections/test/'+id)
       .end(function(e, res){
         // console.log(res.body)
-        expect(e).to.eql(null)
-        expect(typeof res.body).to.eql('object')
-        expect(res.body.msg).to.eql('success')
+        expect(e).to.eql(null);
+        expect(typeof res.body).to.eql('object');
+        expect(res.body.msg).to.eql('success');
         done()
       })
   })
-})
+});
